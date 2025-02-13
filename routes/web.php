e8db790cc4ce983ebb3fafaff563182484d37fc5
Peyin0101 <randi.fernandez-moran@hotmail.com>
2025-02-13 12:26:24 +0100

    yow

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/insert-data', function() {
+Route::get('/insert-data', function () {
 
     $book = new Book;
     $book->title = 'Why is Idris Gay?';
@@ -20,24 +20,21 @@ Route::get('/insert-data', function() {
     $book->save();
 });
 
-Route::get('/books', function() {
-    $books= Book::all();
+Route::get('/books', function () {
+    $books = Book::all();
     return $books;
-    // return view('books', ['books' => $books]);
 });
 
-Route::get('/recent_books', function() {
+Route::get('/recent_books', function () {
     $books = Book::table('books')->where('publication_year', '>', 2000)
-    ->orderBy('publication_year', 'desc')
-    ->get();
+        ->orderBy('publication_year', 'desc')
+        ->get();
     return $books;
-    // return view('books', ['books' => $books]);
 });
 
-Route::get('/aantallen', function() {
+Route::get('/aantallen', function () {
     $books = Book::count();
     $authors = Author::count();
     $genres = Genre::count();
     return view('aantallen', ['books' => $books, 'authors' => $authors, 'genres' => $genres]);
-    // return view('aantallen', compact('books', 'authors', 'genres'));
 });
